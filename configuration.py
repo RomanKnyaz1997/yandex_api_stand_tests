3b2e455d809a568d4e4586069e0ad891c8bd0a9a
Roman <Roman.knyazkov1997@gmail.com>
2023-10-15 11:17:15 +0200

    My commit

---
+++ configuration.py
@@ -0,0 +1,8 @@
+URL_SERVICE = "https://93428a51-0013-438e-bd02-3c20b1f64c55.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+CREATE_USER_PATH = "/api/v1/users/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+
+
